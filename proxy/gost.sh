d755312dcccc468ac05ed093431083f9ab378e69
0x775 <zhangsan01234@qq.com>
2025-08-04 00:09:52 +0800

    Update gost.sh

--- proxy/gost.sh
+++ proxy/gost.sh
@@ -1,5 +1,5 @@
 docker run -d --name secure-proxy \
   -p 1080:1080 -p 3389:8080 \
   ginuerzh/gost \
-  -L "socks5://demo:123456a@:1080?dns=8.8.8.8" \
-  -L "http://demo:123456a@:8080?dns=8.8.8.8"
+  -L "socks5://demo:qazwsx1a@:1080?dns=8.8.8.8,114.114.114.114" \
+  -L "http://demo:qazwsx1aa@:8080?dns=8.8.8.8,114.114.114.114"
