64ab02abe193a44e3633ccd72c7aaae1e577782a
0x775 <zhangsan01234@qq.com>
2025-09-11 22:14:41 +0800

    Update gost.sh

--- proxy/gost.sh
+++ proxy/gost.sh
@@ -1,3 +1,27 @@
+#debian 12
+# 安装基础工具
+apt update && apt install -y ca-certificates curl
+
+# 创建密钥存储目录
+install -m 0755 -d /etc/apt/keyrings
+
+# 获取官方密钥
+curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+
+# 添加官方存储库
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# 更新软件源
+apt update
+
+# 安装docker
+apt install -y docker-ce docker-ce-cli
+
+#运行容器
 docker run -d --name secure-proxy \
   -p 1080:1080 -p 3389:8080 \
   ginuerzh/gost \
