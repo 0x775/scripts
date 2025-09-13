978e61e695cde55ae13c8c8fa1ec29b7ad310259
0x775 <zhangsan01234@qq.com>
2025-09-14 00:51:32 +0800

    Update gost.sh

--- proxy/gost.sh
+++ proxy/gost.sh
@@ -22,7 +22,7 @@ apt update
 apt install -y docker-ce docker-ce-cli
 
 #运行容器
-docker run -d --name secure-proxy \
+docker run -d --restart=always --name secure-proxy \
   -p 1080:1080 -p 3389:8080 \
   ginuerzh/gost \
   -L "socks5://demo:qazwsx1a@:1080?dns=8.8.8.8,114.114.114.114" \
